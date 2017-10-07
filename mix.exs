b1e6841573f29de7cbf653968b5832a722a0ebbd
David Rouchy <drouchy@gmail.com>
2017-10-07 15:40:57 +0100

    configure Circle CI

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule Mathematics.Mixfile do
     ]
   end
 
-  defp elixirc_paths(:travis), do: elixirc_paths(:test)
+  defp elixirc_paths(:ci), do: elixirc_paths(:test)
   defp elixirc_paths(:test),   do: ["lib", "web", "test/support"]
   defp elixirc_paths(_),       do: ["lib", "web"]
 
@@ -25,9 +25,10 @@ defmodule Mathematics.Mixfile do
   defp deps do
     [
       {:mix_test_watch, "~> 0.5.0"},
-      {:eqc_ex, "~> 1.4", only: [:dev, :travis, :test]},
-      {:excoveralls, "~> 0.7.4", only: [:test, :travis]},
-      {:dialyxir, "~> 0.5", only: [:dev, :travis], runtime: false}
+      {:eqc_ex, "~> 1.4", only: [:dev, :ci, :test]},
+      {:excoveralls, "~> 0.7.4", only: [:test, :ci]},
+      {:dialyxir, "~> 0.5", only: [:dev, :ci], runtime: false},
+      {:junit_formatter, "~> 2.0", only: [:test, :ci]}
     ]
   end
 end
